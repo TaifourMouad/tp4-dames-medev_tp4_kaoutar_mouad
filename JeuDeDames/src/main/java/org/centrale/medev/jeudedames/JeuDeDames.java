df418a6a6e378582343dae9e7015b63b2e4c96cb
kaoutar <kaoutarrasmi2002@gmail.com>
2024-12-05 09:14:56 +0100

    création du projet

---
+++ JeuDeDames/src/main/java/org/centrale/medev/jeudedames/JeuDeDames.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.medev.jeudedames;
+
+/**
+ *
+ * @author kaoutar,mouad
+ */
+public class JeuDeDames {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
