54a7fea77332299f9e0a78a4d1dad34b8bfd899c
Mouad <mouadtaifour01@gmail.com>
2024-12-12 11:41:39 +0100

    testPrise (pas complet)

--- JeuDeDames/src/test/java/org/centrale/medev/jeudedames/PionTest.java
+++ JeuDeDames/src/test/java/org/centrale/medev/jeudedames/PionTest.java
@@ -32,7 +32,7 @@ public class PionTest {
         Plateau plateau = new Plateau(); 
         plateau.init();
         Pion pionBlanc = new Pion(new Point2D(1,1), "B");
-        //Pion pionNoir = new Pion(new Point2D(6,6), "N"); 
+        Pion pionNoir = new Pion(new Point2D(6,6), "N"); 
         
         pionBlanc.deplacer(2, 2, plateau);
         assertEquals(2, pionBlanc.getPos().getX()); 
